4f7b609d5d1614ac3f9a1ba91b365f0a3e36c295
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-08-18 19:42:13 +0300

    Add grid pane

--- src/main/java/com/example/javafx/HelloApplication.java
+++ src/main/java/com/example/javafx/HelloApplication.java
@@ -14,6 +14,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         GridPane gridPane = new GridPane();
+        gridPane.setMinSize(400, 200);
         stage.setTitle("JavaFX Basic Application");
         stage.setScene(scene);
         stage.show();
